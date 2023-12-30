25d149d8e8614eb34fe68ac8053245dd5768c7e6
Joao Andrade <joaovandrade30@gmail.com>
2023-12-30 01:51:27 -0300

    sending code

---
+++ arrays/different_arrays.r
@@ -0,0 +1,15 @@
+Jane <- c('Bolsa', 'Blusa', 'Jaqueta')
+Robert <- c('Bolsa', 'Roupão', 'Bota', 'Calça')
+
+
+
+# Unindo vetores
+print(union(Jane, Robert))
+
+# Intersecção
+print(intersect(Jane, Robert))
+
+
+# Diferença
+print(setdiff(Jane, Robert))
+print(setdiff(Robert, Jane))
