f417d8459c6ca3910f8845ecb8865c7ed6211265
Joao Andrade <joaovandrade30@gmail.com>
2023-12-30 01:43:42 -0300

    Sending my codes

---
+++ arrays/array.r
@@ -0,0 +1,37 @@
+# Vetores
+
+
+ages <- c(19, 29, 35, 32)
+
+
+print(sum(ages))
+
+
+birthday <- c('April', 'September', 'January', 'December')
+
+print(birthday)
+
+
+# ages_example <- scan()
+
+print(ages[1])
+print(ages[c(1, 3)])
+print(ages[c(1:3)])
+
+
+
+ages[1] <- 25
+
+print(ages[1])
+
+
+ages[-1]
+
+
+print(ages[])
+
+
+ages <- ages[-1]
+
+
+print(ages[])
---
+++ arrays/sales.r
@@ -0,0 +1,15 @@
+vendas_jan <- c(10, 15, 12, 15, 14, 11)
+vendas_fev <- c(10, 18, 22, 11, 12, 9)
+vendas_mar <- c(8, 12, 12, 7, 19, 18)
+
+
+print(vendas_jan + vendas_fev + vendas_mar)
+print(vendas_jan - vendas_fev)
+print(sum(vendas_jan + vendas_fev + vendas_mar))
+
+print('Inserindo valor por posição no vetor')
+
+print(vendas_jan + 10)
+print(vendas_jan * 10)
+print(vendas_jan ^ 10)
+
