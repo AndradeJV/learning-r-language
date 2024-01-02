205334fe9d5059a387585b3276b6164010b7d1e0
Joao Andrade <joao.andrade@ecs.com.br>
2024-01-02 16:20:53 -0300

    working with headquarters

---
+++ headquarters/learning.r
@@ -0,0 +1,25 @@
+client <- c("Carlos", "Pedro", "Maria")
+items <- c(23, 45, 12)
+
+
+print(client)
+print(items)
+
+
+sales <- cbind(client, items)
+
+print(sales)
+
+
+sales_two <- rbind(client, items)
+
+## Access and reference
+
+print(sales[1, 2])
+print(sales[1,])
+print(sales[, 2])
+
+print('--')
+print(sales[1:2,])
+
+print(sales[c(1, 3),])
